0e5ab330f6f2e3e339cd548595399a7e32ecaddc
gbasiladze <basiladze@mail.ru>
2023-01-11 11:50:30 +0300

    add base entity for attribute id

---
+++ model/src/main/java/com/goganesh/bookshop/model/domain/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.goganesh.bookshop.model.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}
